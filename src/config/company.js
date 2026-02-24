d329d2340e61da3b155e3a52cce700e98a769b7e
xuanlin129 <xuan.lin129@gmail.com>
2026-02-24 21:17:52 +0800

    chore: email

--- src/config/company.js
+++ src/config/company.js
@@ -4,7 +4,7 @@ export default {
   tel: '02-8292-1781',
   fax: '02-8292-6304',
   address: '新北市鶯歌區鶯桃路365巷37號',
-  email: 'yichieh551314@gmail.com',
+  email: 'shd27797775@gmail.com',
   uni_no: '27797775',
   line: 'https://lin.ee/8HxkoGPw',
   facebook: 'https://www.facebook.com/share/1EbrbTH7GW/?mibextid=wwXIfr',
