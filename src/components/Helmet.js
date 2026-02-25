142531e061430b8ad4c4df668e88f720ad9f2ea9
xuanlin129 <xuan.lin129@gmail.com>
2026-02-25 15:39:45 +0800

    chore: website title

--- src/components/Helmet.js
+++ src/components/Helmet.js
@@ -4,8 +4,8 @@ import { Helmet } from 'react-helmet-async';
 function PageWithHelmet({ title, children }) {
   const fullTitle =
     title === '首頁'
-      ? '興樺德興業有限公司(新北烤漆/專業粉體塗裝)'
-      : `${title} | 興樺德興業有限公司(新北烤漆/專業粉體塗裝)`;
+      ? '興樺德興業有限公司（新北烤漆|專業粉體塗裝|皮膜前處理'
+      : `${title} | 興樺德興業有限公司（新北烤漆|專業粉體塗裝|皮膜前處理`;
 
   return (
     <>
