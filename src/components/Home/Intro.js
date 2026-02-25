f459eaaee70157ca6b4abaa1a947417fc58f0bcc
xuanlin129 <xuan.lin129@gmail.com>
2026-02-25 19:26:11 +0800

    chore: intro image

--- src/components/Home/Intro.js
+++ src/components/Home/Intro.js
@@ -44,8 +44,8 @@ function Intro() {
         <Row gutter={[48, 48]} wrap align="middle" justify="space-between">
           <Col xs={{ span: 24, order: 2 }} md={{ span: 10, order: 0 }} lg={10}>
             <div className="image-area">
-              <img src="https://picsum.photos/1920/1080/?random=2" />
-              <img src="https://picsum.photos/1920/1080/?random=3" />
+              <img src={new URL('@/assets/intro-01.jpg', import.meta.url).href} />
+              <img src={new URL('@/assets/intro-02.jpg', import.meta.url).href} />
             </div>
           </Col>
           <Col xs={24} md={14} lg={12}>
@@ -87,19 +87,22 @@ const Wrapper = styled.section`
     width: 100%;
     position: relative;
     display: flex;
-    flex-direction: column;
     gap: var(--gap);
 
     img {
-      width: 90%;
-      aspect-ratio: 2/1;
+      width: calc(50% - var(--gap) / 2);
+      aspect-ratio: 1/2;
       object-fit: cover;
       display: block
       text-align: center;
       border-radius: 10rem;
 
+      &:first-child {
+        margin-bottom: 30%;
+      }
+
       &:last-child {
-        margin-left: auto;
+        margin-top: 30%;
       }
     }
   }
@@ -144,23 +147,6 @@ const Wrapper = styled.section`
   }
 
   @media (min-width: 576px) {
-    & .image-area {
-      flex-direction: row;
-
-      img {
-        width: calc(50% - var(--gap) / 2);
-        aspect-ratio: 1/2;
-
-        &:first-child {
-          margin-bottom: 30%;
-        }
-
-        &:last-child {
-          margin-top: 30%;
-        }
-      }
-    }
-
     & .content {
       & > h2 {
         font-size: 2rem;
